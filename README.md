6d3a8b5290258000a4d1719354a609ea45ffecc7
Giovanni <lugk4l@gmail.com>
2021-02-07 15:59:31 -0500

    script implemented

--- README.md
+++ README.md
@@ -4,7 +4,11 @@
 
 - The Portfolio page displays a gallery with recent works, and clicking on the image will link to the deployed application;
 
-- Added a script to diplay a "quote of the day"
+- Added a script to diplay a "quote of the day" in the index page, an external API is used, this particular API has a limited number of daily calls,
+
+- In the portfolio added a side section with a list of what I'm working on, it will be updated when needed;
+
+-
 
 [Repository](https://github.com/Gio86krt)\
 [Portfolio-GiovanniOrlando](https://gio86krt.github.io/Responsiveness-Portfolio/)
--- script.js
+++ script.js
@@ -1,11 +1,73 @@
+const quotes = [
+  {
+    text:
+      "All our dreams can come true, if we have the courage to pursue them.",
+    author: " Walt Disney",
+  },
+  {
+    text: "The secret of getting ahead is getting started.",
+    author: "Mark Twain",
+  },
+  {
+    text:
+      "I’ve missed more than 9,000 shots in my career. I’ve lost almost 300 games. 26 times I’ve been trusted to take the game winning shot and missed. I’ve failed over and over and over again in my life and that is why I succeed.",
+    author: "Michael Jordan",
+  },
+  {
+    text:
+      "Don’t limit yourself. Many people limit themselves to what they think they can do. You can go as far as your mind lets you. What you believe, remember, you can achieve.",
+    author: "Mary Kay Ash",
+  },
+  {
+    text:
+      "The best time to plant a tree was 20 years ago. The second best time is now.",
+    author: "Chinese Proverb",
+  },
+  { text: "Only the paranoid survive.", author: "Andy Grove" },
+  {
+    text: "It’s hard to beat a person who never gives up.",
+    author: "Babe Ruth",
+  },
+  {
+    text:
+      "I wake up every morning and think to myself, ‘how far can I push this company in the next 24 hours.",
+    author: "Leah Busque",
+  },
+  {
+    text:
+      "If people are doubting how far you can go, go so far that you can’t hear them anymore.",
+    author: "Michele Ruiz",
+  },
+  {
+    text:
+      "We need to accept that we won’t always make the right decisions, that we’ll screw up royally sometimes – understanding that failure is not the opposite of success, it’s part of success.",
+    author: "Arianna Huffington",
+  },
+  {
+    text:
+      "Write it. Shoot it. Publish it. Crochet it, sauté it, whatever. MAKE.",
+    author: "Joss Whedon",
+  },
+];
+
 async function getData() {
   let url = `https://quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com/quote?rapidapi-key=4ce060cc20msh979307ea98f66b2p1d7958jsnbdd5527db2b6`;
   let result = await fetch(url).then((res) => res.json());
 
-  document.querySelector(
-    ".quote"
-  ).innerHTML += `${result.text} <br>  <p> ${result.author}</p><p class="cit">Powered by: https://bestinfopoint.com/</p>`;
-  console.log(result);
+  if (!result.text) {
+    let index = Math.floor(Math.random() * quotes.length);
+    let text = quotes[index].text;
+    let author = quotes[index].author;
+    document.querySelector(
+      ".quote"
+    ).innerHTML += `${text} <br>  <p> ${author}</p><p class="cit">Powered by: https://bestinfopoint.com/</p>`;
+    console.log(result);
+  } else {
+    document.querySelector(
+      ".quote"
+    ).innerHTML += `${result.text} <br>  <p> ${result.author}</p><p class="cit">Powered by: https://bestinfopoint.com/</p>`;
+    console.log(result);
+  }
 }
 
 window.onload = getData();
